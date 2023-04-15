211aa488e77ec9d10a732eaaa00c213c9dbf2d49
Mahender_Kumar <mksb1947@gmail.com>
2023-04-15 17:35:39 +0530

    Initial commit

---
+++ app/src/main/java/com/example/memer/MainActivity.kt
@@ -0,0 +1,16 @@
+package com.example.memer
+
+import android.app.DownloadManager.Request
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import com.android.volley.toolbox.StringRequest
+import com.android.volley.toolbox.Volley
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+
+}
